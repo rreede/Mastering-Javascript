d87be5cae1dc35855133e9acfbcc1325b03085b1
Rene Reede <rene.reede@gmail.com>
2023-02-11 14:15:13 +0200

    Basics 1
    - Values and Variables
    - Data Types
    - Basic Operators
    - Operator Precedense

---
+++ assignments.js
@@ -0,0 +1,36 @@
+const country = "Estonia";
+const continent = "Europe";
+let population = 1.2;
+
+console.log(country, continent, population);
+
+const isIsland = "false";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof population);
+
+const language = "Estonian";
+
+const populationHalf = population / 2;
+
+population += 1;
+console.log(population);
+
+const finlandPopulation = 6;
+const averagePopulation = 33;
+
+console.log(finlandPopulation > population);
+console.log(population > averagePopulation);
+
+const description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " million people speak " +
+  language;
+
+console.log(description);
---
+++ script.js
@@ -0,0 +1,92 @@
+/*
+
+let js = "amazing";
+console.log(40 + 4 - 42);
+
+console.log("Jonas");
+
+let firstName = "Bob";
+Variable name conventions 
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let lastName = "cat";
+
+console.log(firstName + " " + lastName);
+
+console.log(typeof 0);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// Math operators
+
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Rene";
+const lastName = "Reede";
+
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+
+let x = 10 + 5;
+x += 10; // x = x + 10;
+
+x *= 4; // x = x * 4
+x++; // x = x + 1;
+x--; // x = x - 1;
+console.log(x);
+
+// Comparison operator
+
+console.log(ageJonas > ageSarah); // <, >, >=, <=
+
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+
+log(x, y);
+
+constAverageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, constAverageAge);
+*/
+
+const MarkHeight = 1.69;
+const MarkWeight = 78;
+
+const JohnHeight = 1.95;
+const JohnWeight = 92;
+
+const MarkBMI = MarkWeight / MarkHeight ** 2;
+const JohnBMI = JohnWeight / JohnHeight ** 2;
+
+const markHigherBMI = MarkBMI > JohnBMI;
+console.log(markHigherBMI);
