0328515412654341e19dfe7b7f6f756b67f2d13a
Rene Reede <rene.reede@gmail.com>
2023-02-27 16:12:38 +0200

    Football betting game

---
+++ Football-betting-app/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const printGoals = function (...players) {
+  console.log(...players, `a total of ${game.scored.length} goals were scored`);
+};
+
+const [players1, players2] = game.players;
+
+const [gk, ...fieldPlayers] = players1;
+
+const allPlayers = [...players1, ...players2];
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+const { team1, x: draw, team2 } = game.odds;
+
+console.log(draw);
+
+printGoals(...players1, ...players2);
+
+team1 < team2 && console.log("Team 1 is more likely to win");
